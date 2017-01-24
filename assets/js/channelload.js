2142ba95b3f8feaaf15bc21f51822b43eb957d18
alex@netwaveinteractive.com <alex.rosenkranz@gmail.com>
2017-01-23 19:57:45 -0500

    removed sc auth for now

--- assets/js/channelload.js
+++ assets/js/channelload.js
@@ -1,140 +1,131 @@
 $(document).ready(function() {
 
-  var channelId;
-  var counter = 0;
-  var playlist = [];
-  var artists = [];
-  var trackNumber;
-  var playlistName;
-  var audio = document.querySelector('audio');
+    var channelId;
+    var counter = 0;
+    var playlist = [];
+    var artists = [];
+    var trackNumber;
+    var playlistName;
+    var audio = document.querySelector('audio');
 
-  dataRef.ref('channels').on('child_added', function(childSnapshot) {
-    // console.log(childSnapshot.val());
-    $('.channelList').prepend('<div class="column column-block channelButton" data-equalizer-watch data-channel="' + childSnapshot.key + '">' + childSnapshot.val().channelName + '</div>');
-  }, function(errorObject) {
-    console.log("Errors handled: " + errorObject.code);
-  });
-
-  $(document).on('click', '.soundcloud-btn', function() {
-    // initiate auth popup
-    SC.connect().then(function() {
-      return SC.get('/me');
-    }).then(function(me) {
-      console.log(me);
+    dataRef.ref('channels').on('child_added', function(childSnapshot) {
+        // console.log(childSnapshot.val());
+        $('.channelList').prepend('<div class="column column-block channelButton" data-equalizer-watch data-channel="' + childSnapshot.key + '">' + childSnapshot.val().channelName + '</div>');
+    }, function(errorObject) {
+        console.log("Errors handled: " + errorObject.code);
     });
-  })
 
-  // When a channel is clicked, 
-  $(document).on('click', '.channelButton', function() {
-    $('.current-time').css('opacity', '1');
-    $('.channelList').fadeOut('slow');
-    $('.show-channels').html('Show Channels');
-    $('.show-button').html('Close Player');
-    $('.logo').addClass('logo-small');
-    $('.track-player').addClass('is-open');
-    channelId = $(this).data('channel').trim();
-    playlistName = "";
-    artists = [];
-    playlist = [];
-    trackNumber = 0;
-    console.log(trackNumber);
-    console.log(channelId);
-    loadChannel();
+    // When a channel is clicked, 
+    $(document).on('click', '.channelButton', function() {
+        $('.current-time').css('opacity', '1');
+        $('.channelList').fadeOut('slow');
+        $('.show-channels').html('Show Channels');
+        $('.show-button').html('Close Player');
+        $('.logo').addClass('logo-small');
+        $('.track-player').addClass('is-open');
+        channelId = $(this).data('channel').trim();
+        playlistName = "";
+        artists = [];
+        playlist = [];
+        trackNumber = 0;
+        console.log(trackNumber);
+        console.log(channelId);
+        loadChannel();
 
-  });
+    });
 
-  function playTracks() {
-    audio.currentTime = 0;
-    var trackUrl = playlist[trackNumber];
-    var songInfo = artists[trackNumber];
-    $('.song-info').html(songInfo);
-    $('.playlist-info').html('NOW PLAYING: ' + playlistName);
-    $(document).prop('title', playlistName + ' // Gargantuan.FM')
-    $('#play-pause').removeClass('fi-play').addClass('fi-pause');
-    if (trackNumber < playlist.length) {
+    function playTracks() {
+        audio.currentTime = 0;
+        var trackUrl = playlist[trackNumber];
+        var songInfo = artists[trackNumber];
+        $('.song-info').html(songInfo);
+        $('.playlist-info').html('NOW PLAYING: ' + playlistName);
+        $(document).prop('title', playlistName + ' // Gargantuan.FM')
+        $('#play-pause').removeClass('fi-play').addClass('fi-pause');
+        if (trackNumber < playlist.length) {
 
-      $('audio').attr("src", trackUrl + "?client_id=8761e61199b55df39ee27a92f2771aeb");
-      setTimeout(function() {
-        if ($('audio').get(0).paused) {
-          $('audio').get(0).play();
+            $('audio').attr("src", trackUrl + "?client_id=8761e61199b55df39ee27a92f2771aeb");
+            setTimeout(function() {
+                if ($('audio').get(0).paused) {
+                    $('audio').get(0).play();
+                }
+            }, 150);
+
+            console.log("Track Number: " + trackNumber);
+
+            $('audio').get(0).onended = function() {
+                nextSong();
+            }
+        } else {
+            replay();
         }
-      }, 150);
+    };
 
-      console.log("Track Number: " + trackNumber);
+    audio.addEventListener('error',nextSong);
 
-      $('audio').get(0).onended = function() {
+    //function to play previous song
+    function prevSong() {
+        if (trackNumber > 0) {
+            trackNumber--;
+            setTimeout(playTracks, 150);
+        } else {
+            replay();
+        }
+    };
+
+    //replays playlist
+    function replay() {
+        $('audio').get(0).pause();
+        trackNumber = 0;
+        playTracks();
+    };
+
+    //plays previous track when prevButton clicked
+    $(document).on('click', '#prevButton', function() {
+        $('audio').get(0).pause();
+        $('audio').get(0).currentTime = 0;
+        prevSong();
+    });
+
+    //plays next track when skipButton clicked
+    $(document).on('click', '#skipButton', function() {
+        $('audio').get(0).pause();
+        $('audio').get(0).currentTime = 0;
+        nextSong();
+    });
+
+    //function to play next song
+    function nextSong() {
+      trackNumber++;
+      playTracks();
+    };
+
+    $(document).keyup(function(e) {
+      console.log(e);
+      if (e.keyCode === 39) { 
         nextSong();
       }
-    } else {
-      replay();
-    }
-  };
-
-  audio.addEventListener('error', nextSong);
-
-  //function to play previous song
-  function prevSong() {
-    if (trackNumber > 0) {
-      trackNumber--;
-      setTimeout(playTracks, 150);
-    } else {
-      replay();
-    }
-  };
-
-  //replays playlist
-  function replay() {
-    $('audio').get(0).pause();
-    trackNumber = 0;
-    playTracks();
-  };
-
-  //plays previous track when prevButton clicked
-  $(document).on('click', '#prevButton', function() {
-    $('audio').get(0).pause();
-    $('audio').get(0).currentTime = 0;
-    prevSong();
-  });
-
-  //plays next track when skipButton clicked
-  $(document).on('click', '#skipButton', function() {
-    $('audio').get(0).pause();
-    $('audio').get(0).currentTime = 0;
-    nextSong();
-  });
-
-  //function to play next song
-  function nextSong() {
-    trackNumber++;
-    playTracks();
-  };
-
-  $(document).keyup(function(e) {
-    console.log(e);
-    if (e.keyCode === 39) {
-      nextSong();
-    }
-    if (e.keyCode === 37) {
-      prevSong();
-    }
-  })
-
-  // Pulls the selected channel's info and passes it into our audio element 
-  function loadChannel() {
-    dataRef.ref('channels/' + channelId).once('value').then(function(snapshot) {
-      playlistName = snapshot.val().channelName;
-      //pulls selected channel's tracks 
-      var channel = snapshot.val();
-      var tracks = channel['tracks'];
-
-      //pushes streaming urls into an array
-      for (var i = 0; i < tracks.length; i++) {
-        artists.push(tracks[i].artist + ' - ' + tracks[i].trackName);
-        playlist.push(tracks[i].url);
-      };
-
-      //use html5 audio to play tracks
-      playTracks();
+     if (e.keyCode === 37) {
+        prevSong();
+     }
     })
-  };
+
+    // Pulls the selected channel's info and passes it into our audio element 
+    function loadChannel() {
+        dataRef.ref('channels/' + channelId).once('value').then(function(snapshot) {
+            playlistName = snapshot.val().channelName;
+            //pulls selected channel's tracks 
+            var channel = snapshot.val();
+            var tracks = channel['tracks'];
+
+            //pushes streaming urls into an array
+            for (var i = 0; i < tracks.length; i++) {
+                artists.push(tracks[i].artist + ' - ' + tracks[i].trackName);
+                playlist.push(tracks[i].url);
+            };
+
+            //use html5 audio to play tracks
+            playTracks();
+        })
+    };
 });
